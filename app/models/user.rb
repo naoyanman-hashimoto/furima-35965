a4a2044480fc0fbe2abde597a9a1b3cce76bfab7
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-17 22:28:26 +0900

    バリデーションの深掘り実装(全角、カナ、英数字混同)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :birthday, presence: true
+  valid_name_regex = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+  valid_name_kana_regex = /\A[ァ-ヶー]+\z/
+  valid_password_regex = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+
+  with_options presence: true do
+    validates_format_of :password, with: valid_password_regex, message: 'is invalid. Include both letters and numbers.'
+    validates :nickname
+    validates :first_name, format: { with: valid_name_regex, message: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: valid_name_regex, message: 'is invalid. Input full-width characters.' }
+    validates :kana_first_name, format: { with: valid_name_kana_regex, message: 'is invalid. Input full-width katakana characters.' }
+    validates :kana_last_name, format: { with:  valid_name_kana_regex, message: 'is invalid. Input full-width katakana characters.' }
+    validates :birthday
+  end
 end

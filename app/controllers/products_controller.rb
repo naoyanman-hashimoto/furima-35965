197fe26ea07870d13904654961843e69f0a79492
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-18 19:43:56 +0900

    ルーティングとアクションの設定、プライベートメソッドの誤字修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,14 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   private
-  def prototype_params
+  def product_params
     params.require(:product).permit(:image).merge(user_id: current_user.id)
   end
 end

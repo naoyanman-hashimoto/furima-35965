3053830d70c09be4b930794d850a09311c2e4b4b
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-18 19:39:46 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :shipment_source_id
+    validates :time_required_id
+    validates :price
+    validates :image
+  end
 end

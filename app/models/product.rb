f15b8a394773610f6252ebab029ea9d0c7deda23
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-18 23:18:47 +0900

    ActiveHashを用いたプルダウンの実装(商品の状態、配送料の負担、発送元の地域、発送までの日数)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :shipment_source
+  belongs_to :time_required
+
   belongs_to :user
   has_one_attached :image
 
@@ -15,5 +20,9 @@ class Product < ApplicationRecord
     validates :price
     validates :image
   end
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :burden_id, numericality: { other_than: 1 }
+  validates :shipment_source_id, numericality: { other_than: 1 }
+  validates :time_required_id, numericality: { other_than: 1 }
 end

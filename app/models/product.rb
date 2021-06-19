d5717a5a921b3c331b957130dbf1c08da1c42607
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-19 14:43:04 +0900

    バリデーションの整理と価格の上下限設定を実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,17 +12,16 @@ class Product < ApplicationRecord
   with_options presence: true do
     validates :product_name
     validates :description
+    validates :price,
+               numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1,message: "can't be blank" } do
     validates :category_id
     validates :status_id
     validates :burden_id
     validates :shipment_source_id
     validates :time_required_id
-    validates :price
-    validates :image
   end
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :shipment_source_id, numericality: { other_than: 1 }
-  validates :time_required_id, numericality: { other_than: 1 }
 end

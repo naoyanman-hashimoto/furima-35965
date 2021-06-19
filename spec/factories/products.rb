7e9f36769d4d2cc0d2e0d5f34be17fe6f54d7f1a
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-19 16:35:49 +0900

    exampleの整理と正常系テストの実装

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
+    category_id {2}
+    status_id {2}
+    burden_id {2}
+    shipment_source_id {2}
+    time_required_id {2}
+
+    price {1000}
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @product = FactoryBot.build(:product)
+    end
+    describe '商品出品機能' do
+      context '商品出品できる場合' do
+        it '必要な情報を適切に入力すると、商品情報がデータベースに保存されること' do
+          expect(@product).to be_valid
+        end
+      end
+
+      context '商品出品できない場合' do
+        it '商品画像を1枚つけることが必須であること(imageが空では保存できない)' do
+        end
+        it '商品名が必須であること(product_nameが空では保存できない)' do
+        end
+        it '商品の説明が必須であること(descriptionが空では保存できない)' do
+        end
+        it 'カテゴリーの情報が必須であること(category_idが未選択では保存できない)' do
+        end
+        it '商品の状態の情報が必須であること(status_idが未選択では保存できない)' do
+        end
+        it '配送料の負担の情報が必須であること(burden_idが未選択では保存できない)' do
+        end
+        it '発送元の地域の情報が必須であること(shipment_source_idが未選択では保存できない)' do
+        end
+        it '発送までの日数の情報が必須であること(time_required_idが未選択では保存できない)' do
+        end
+        it '価格の情報が必須であること(priceが未選択では保存できない)' do
+        end
+        it '価格は、¥300~¥9,999,999の間のみ保存可能であること(¥300以下では保存できない)' do
+        end
+        it '価格は、¥300~¥9,999,999の間のみ保存可能であること(¥9,999,999以上では保存できない)' do
+        end
+        it '価格は半角数値のみ保存可能であること(漢数字では保存できない)' do
+        end
+        it '価格は半角数値のみ保存可能であること(全角数値では保存できない)' do
+        end
+      end
+    end
+  end
 end

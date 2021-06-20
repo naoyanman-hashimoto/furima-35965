f4a5a943522017e1fe245f79743b9400be4683e0
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-20 12:44:22 +0900

    編集した内容を更新して保存する機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,9 +23,12 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
   end
 
   private

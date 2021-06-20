28240f2916c426f5190a3751c91fac113dd3b57c
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-20 14:17:40 +0900

    編集後に遷移するページを詳細ページに変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to root_path
+      redirect_to product_path
     else
       render :edit
     end

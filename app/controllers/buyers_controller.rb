1b314490dd60a47a2872ec8ddce3450c087a84df
naoyanman-hashimoto <rkrbhssn@gmail.com>
2021-06-21 14:38:30 +0900

    buyersテーブルとshipping_addressesテーブルにデータを保存できるように設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,4 +2,23 @@ class BuyersController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
   end
+
+  def new
+  end
+
+  def create
+    @buyer = Buyer.create(buyer_params)
+    ShippingAddress.create(shipping_address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def buyer_params
+    params.permit(:user_id, :product_id).merge(user_id: current_user.id)
+  end
+
+  def shipping_address_params
+    params.permit(:postal_code, :shipment_source_id, :municipalities, :address, :building_name, :phone_number).merge(buyer_id: @buyer.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "products#index"
 
   resources :products do
-    resources :buyers, only: [:index]
+    resources :buyers, only: [:index, :new, :create]
   end
 
 end
